bf2e4d02738f23fafdb37cca345b8518153a841b
joey57 <njorogejoey5@gmail.com>
2022-03-09 13:36:59 +0300

    added notes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-var add = function(number1, number2) {
+//business logic
+  var add = function(number1, number2) {
   return number1 + number2;
   };
   var subtract = function(number1, number2) {
@@ -10,13 +11,13 @@ var add = function(number1, number2) {
   var divide = function(number1, number2) {
   return number1 / number2;
   };
-  
+  //user interface
   $(document).ready(function() {
-  $("form#add").submit(function(event) {
-  event.preventDefault();
-  var number1 = parseInt($("#add1").val());
-  var number2 = parseInt($("#add2").val());
-  var result = add(number1, number2);
-  $("#output").text(result);
-  });
+    $("form#add").submit(function(event) {
+      event.preventDefault();
+      var number1 = parseInt($("#add1").val());
+      var number2 = parseInt($("#add2").val());
+      var result = add(number1, number2);
+      $("#output").text(result);
+    });
   });
